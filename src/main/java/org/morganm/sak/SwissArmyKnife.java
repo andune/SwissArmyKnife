0598704dc3e37d18b8a9f31d3d5e34a86d59bb96
Mark Morgan <morganm.qc@gmail.com>
2012-03-19 23:42:26 -0700

    First commit

---
+++ src/main/java/org/morganm/sak/SwissArmyKnife.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.morganm.sak;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author morganm
+ *
+ */
+public class SwissArmyKnife extends JavaPlugin {
+	@Override
+	public void onEnable() {
+	}
+	
+	@Override
+	public void onDisable() {
+	}
+}
